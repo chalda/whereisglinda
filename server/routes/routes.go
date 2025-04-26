da5313db1505e08c2426c5c7c58c076a91e33c3c
Alex Chaldyshev <alex.chaldyshev@gmail.com>
2025-04-26 02:28:25 -0400

    ignore stuff; reenable security headers

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -50,10 +50,10 @@ func (z zerologRecoveryLogger) Println(args ...interface{}) {
 
 func securityHeadersMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//	w.Header().Set("X-Frame-Options", "SAMEORIGIN")
+		w.Header().Set("X-Frame-Options", "SAMEORIGIN")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
-		//	w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
+		w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
 		w.Header().Set("Content-Security-Policy", "default-src 'self'")
 		w.Header().Set("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
 		next.ServeHTTP(w, r)
